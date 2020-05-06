44a15c037ba70a0ab9512f700b8be27c3e5768d3
xue <1259996266@qq.com>
2020-05-06 16:02:47 +0800

    优化作者详情页，视频列表加载

--- src/components/writer/VideoList.vue
+++ src/components/writer/VideoList.vue
@@ -9,13 +9,6 @@
           <v-card-text class="pa-0">
             <v-sheet>
               <v-slide-group multiple show-arrows>
-                <!--<v-slide-item class="pa-2 ma-2 text-left pointer">
-                    <v-card max-width="160" width="160px">
-                      <v-img src="http://i0.hdslb.com/bfs/archive/2c2a95104bee9afa6ac278d476c2640af9763247.jpg" height="100px"></v-img>
-                      <div class="cards-title mt-1 fs_14">纽约市长三连怼：特朗普，防控疫情你都干了些啥？</div>
-                      <div class="text-right mt-2"><v-icon small>mdi-play-circle-outline</v-icon> 17689</div>
-                    </v-card>
-                </v-slide-item>-->
                 <v-slide-item class="pa-2 ma-2 text-left pointer" v-for="(item,index) in newVideoList" :key="index">
                   <v-card max-width="160" width="160px" @click="goTo(item.aid)">
                     <v-img :src="item.pic" height="100px"></v-img>
@@ -23,8 +16,10 @@
                     <div class="text-right"><v-icon small>mdi-play-circle-outline</v-icon> {{item.view}}</div>
                   </v-card>
                 </v-slide-item>
-
               </v-slide-group>
+              <v-overlay class="loader-box" :value="newOverlay" absolute color="#fff" opacity="0.3">
+                <div class="loader"></div>
+              </v-overlay>
             </v-sheet>
           </v-card-text>
         </v-card>
@@ -39,13 +34,6 @@
           <v-card-text class="pa-0">
             <v-sheet>
               <v-slide-group multiple show-arrows>
-                <!--<v-slide-item class="pa-2 ma-2 text-left">
-                  <v-card max-width="160" width="160px">
-                    <v-img src="http://i0.hdslb.com/bfs/archive/2c2a95104bee9afa6ac278d476c2640af9763247.jpg" height="100px"></v-img>
-                    <div class="cards-title mt-1 fs_14">纽约市长三连怼：特朗普，防控疫情你都干了些啥？</div>
-                    <div class="text-right mt-2"><v-icon small>mdi-play-circle-outline</v-icon> 17689</div>
-                  </v-card>
-                </v-slide-item>-->
                 <v-slide-item class="pa-2 ma-2 text-left pointer" v-for="(item,index) in hotVideoList" :key="index">
                   <v-card max-width="160" width="160px" @click="goTo(item.aid)">
                     <v-img :src="item.pic" height="100px"></v-img>
@@ -53,8 +41,10 @@
                     <div class="text-right"><v-icon small>mdi-play-circle-outline</v-icon> {{item.view}}</div>
                   </v-card>
                 </v-slide-item>
-
               </v-slide-group>
+              <v-overlay class="loader-box" :value="hotOverlay" absolute color="#fff" opacity="0.3">
+                <div class="loader"></div>
+              </v-overlay>
             </v-sheet>
           </v-card-text>
         </v-card>
@@ -77,6 +67,8 @@
       return {
         newVideoList: undefined,
         hotVideoList: undefined,
+        newOverlay: true,
+        hotOverlay: true,
       }
     },
     methods: {
@@ -100,8 +92,10 @@
           let data = r.data;
           if(data.success){
             if(sort){
+              self.hotOverlay = false;
               self.hotVideoList = data.data.data;
             }else {
+              self.newOverlay = false;
               self.newVideoList = data.data.data;
             }
           }
@@ -154,5 +148,89 @@
     -webkit-line-clamp: 2;
     overflow: hidden;
     height: 41px;
+  }
+  .pa-0{
+    position: relative;
+    min-height: 195px;
+  }
+  .loader:before{
+    content: "";
+    position: absolute;
+    top: 0px;
+    left: -25px;
+    height: 12px;
+    width: 12px;
+    border-radius: 12px;
+    -webkit-animation: loaderg 3s ease-in-out infinite;
+    animation: loaderg 3s ease-in-out infinite;
+  }
+
+  .loader{
+    position: relative;
+    width: 12px;
+    height: 12px;
+    top: 46%;
+    left: 46%;
+    border-radius: 12px;
+    -webkit-animation: loaderm 3s ease-in-out infinite;
+    animation: loaderm 3s ease-in-out infinite;
+  }
+
+
+  .loader:after{
+    content: "";
+    position: absolute;
+    top: 0px;
+    left: 25px;
+    height: 10px;
+    width: 10px;
+    border-radius: 10px;
+    -webkit-animation: loaderd 3s ease-in-out infinite;
+    animation: loaderd 3s ease-in-out infinite;
+  }
+
+  @-webkit-keyframes loaderg{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, 1);}
+    50%{background-color: rgba(30, 136, 229, .2);}
+    75%{background-color: rgba(30, 136, 229, .2);}
+    100%{background-color: rgba(30, 136, 229, .2);}
+  }
+  @keyframes loaderg{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, 1);}
+    50%{background-color: rgba(30, 136, 229, .2);}
+    75%{background-color: rgba(30, 136, 229, .2);}
+    100%{background-color: rgba(30, 136, 229, .2);}
+  }
+
+  @-webkit-keyframes loaderm{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, .2);}
+    50%{background-color: rgba(30, 136, 229, 1);}
+    75%{background-color: rgba(30, 136, 229, .2);}
+    100%{background-color: rgba(30, 136, 229, .2);}
+  }
+  @keyframes loaderm{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, .2);}
+    50%{background-color: rgba(30, 136, 229, 1);}
+    75%{background-color: rgba(30, 136, 229, .2);}
+    100%{background-color: rgba(30, 136, 229, .2);}
+  }
+
+  @-webkit-keyframes loaderd{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, .2);}
+    50%{background-color: rgba(30, 136, 229, .2);}
+    75%{background-color: rgba(30, 136, 229, 1);}
+    100%{background-color: rgba(30, 136, 229, .2);}
+  }
+  @keyframes loaderd{
+    0%{background-color: rgba(30, 136, 229, .2);}
+    25%{background-color: rgba(30, 136, 229, .2);}
+    50%{background-color: rgba(30, 136, 229, .2);}
+    75%{background-color: rgba(30, 136, 229, 1);}
+    100%{background-color: rgba(30, 136, 229, .2);}
   }
 </style>
